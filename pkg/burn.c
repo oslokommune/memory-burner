c7085d6593ffb064c47531fbfcaae875fa579902
Julius Pedersen <deifyed@tutanota.com>
2022-06-16 08:25:48 +0200

    👌 Store addresses and memset in burn strategy

--- pkg/burn.c
+++ pkg/burn.c
@@ -2,6 +2,8 @@
 #include <stdio.h> // sprintf()
 #include <stdlib.h> // malloc()
 #include <string.h> // strcmp()
+#include <limits.h> // INT_MAX
+#include <stdint.h>
 #include "log.h" // d()
 
 const long megabyte = sizeof(char) * 1024 * 1024;
@@ -29,12 +31,23 @@ void holdMemory(long megabytes) {
 void burnAllMemory(long megabytes) {
 	long chunk = megabytes * megabyte;
 
+	//TOIMPROVE: will overflow before OOM if chunk size is too low
+	int *addresses[100000];
+
+	int addressesIndex = 0;
+
 	char msg[512];
 	sprintf(msg, "Burning all memory, %lu bytes each cycle", chunk);
 
 	d(msg);
 
-	while(1) malloc(chunk);
+	while(1) {
+		addresses[addressesIndex] = (int *) malloc(chunk);
+
+		memset(addresses[addressesIndex], 0, chunk);
+
+		addressesIndex++;
+	}
 }
 
 void burn(char * strategy, long chunkByteSize) {
